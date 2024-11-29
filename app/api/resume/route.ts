30c8628d829749570af04afc5556a50d3eb35d6e
Lanna <yladacs@gmail.com>
2024-11-29 15:35:32 -0300

    fix bugs

--- app/api/resume/route.ts
+++ app/api/resume/route.ts
@@ -4,17 +4,17 @@ import { join } from 'path';
 
 export async function GET() {
   try {
-    // Lê o arquivo PDF da pasta public
-    const pdfPath = join(process.cwd(), 'public', 'resume.pdf');
-    const pdfBuffer = readFileSync(pdfPath);
+    // // Lê o arquivo PDF da pasta public
+    // const pdfPath = join(process.cwd(), 'public', 'resume.pdf');
+    // const pdfBuffer = readFileSync(pdfPath);
     
-    return new NextResponse(pdfBuffer, {
-      headers: {
-        'Content-Type': 'application/pdf',
-        'Content-Disposition': 'attachment; filename="resume.pdf"',
-        'Cache-Control': 'no-cache',
-      },
-    });
+    // return new NextResponse(pdfBuffer, {
+    //   headers: {
+    //     'Content-Type': 'application/pdf',
+    //     'Content-Disposition': 'attachment; filename="resume.pdf"',
+    //     'Cache-Control': 'no-cache',
+    //   },
+    // });
   } catch (error) {
     console.error('Error serving resume:', error);
     return new NextResponse('Error serving resume', { status: 500 });
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,109 @@
 'use client';
 
 import { motion } from 'framer-motion';
+import { useState } from 'react';
 import HeroSection from '@/components/HeroSection';
 import ProjectCard from '@/components/ProjectCard';
 import TechStack from '@/components/TechStack';
 import Footer from '@/components/Footer';
 
 export default function Home() {
+  const initialProjects = [
+    {
+      title: 'Madil Carreiras',
+      description:
+        'Uma plataforma de carreiras responsiva construída com React, Tailwind CSS e Vite.',
+      image:
+        'https://res.cloudinary.com/dggewyuon/image/upload/v1732898636/madil_mrmfai.png',
+      tags: ['React', 'Tailwind', 'Vite'],
+      githubUrl: 'https://github.com/lanroo/madilcarreiras',
+      liveUrl: 'https://madilcarreiras.vercel.app/',
+    },
+    {
+      title: 'Hivex Bank',
+      description:
+        'Landing page moderna para um banco digital usando React, Tailwind e Vite, integrando design responsivo.',
+      image:
+        'https://res.cloudinary.com/dggewyuon/image/upload/v1732898928/hivex_lwworg.jpg',
+      tags: ['React Native', 'React', 'TypeScript', 'Tailwind'],
+      githubUrl: 'https://github.com/yourusername/project2',
+      liveUrl: 'https://hivexbank.netlify.app/',
+    },
+    {
+      title: 'ArbiGenius',
+      description:
+        'Backend API - Plataforma de arbitragem de criptomoedas feita com Python Flask.',
+      image:
+        'https://fenixconsultoria.com.br/admin/cadastros/blog/fotos/1656102160.jpg',
+      tags: ['Python', 'Flask', 'Vue', 'Vite', 'Tailwind'],
+      githubUrl: 'https://github.com/lanroo/ArbiGenius',
+      liveUrl: 'https://project3.com',
+    },
+    {
+      title: 'Automação Whatsapp',
+      description:
+        'Automação para o whatsapp utilizando Python, Flask, Selenium WebDriver e Docker.',
+      image:
+        'https://blog.meugrupo.vip/wp-content/uploads/2022/11/WhatsApp-Automation-for-WordPress-1024x536.png',
+      tags: ['Python', 'Flask', 'Docker', 'Selenium'],
+      githubUrl: 'https://github.com/lanroo/wppautomation',
+    },
+    {
+      title: 'API de Leitura de Medição',
+      description:
+        'API para leitura automatizada de medições de água e gás a partir de imagens enviadas em base64 e gemini.',
+      image:
+        'https://res.cloudinary.com/dggewyuon/image/upload/v1732901357/imagem_vtdzr2.png',
+      tags: ['Node', 'Express', 'TypeScript', 'Docker', 'API Gemini'],
+      githubUrl: 'https://github.com/lanroo/MeasurementReadAPI',
+    },
+    {
+      title: 'Downloader',
+      description:
+        'Downloader Web e Extensão Google de vídeos YouTube criado com Python.',
+      image:
+        'https://res.cloudinary.com/dggewyuon/image/upload/v1732902116/downloader_yenh1y.png',
+      tags: ['Python', 'Flask', 'Socket.IO', 'Bootstrap 4'],
+      githubUrl: 'https://github.com/lanroo/downloadering',
+    },
+  ];
+
+  const additionalProjects = [
+    {
+      title: 'Paint Web - Drawart',
+      description:
+        'Espaço para desenho/pintura estilo Paint. Usando tecnologia canvas JS.',
+      image: 'https://res.cloudinary.com/dggewyuon/image/upload/v1732903352/paint_doyjqh.png',
+      tags: ['Javascript', 'HTML', 'CSS', 'Canvas API'],
+      githubUrl: 'https://github.com/lanroo/DRAWART_',
+      liveUrl: 'https://drawart.vercel.app/',
+    },
+    {
+      title: 'Disaster Alerts',
+      description:
+        'Sistema de alerta de desastres climáticos que monitora riscos em tempo real e envia notificações personalizadas por SMS e e-mail, utilizando dados do OpenWeatherMap, NOAA e NASA.',
+      image: 'https://res.cloudinary.com/dggewyuon/image/upload/v1732904702/alert_bjwygt.png',
+      tags: ['React', 'Express', 'ESlint'],
+      githubUrl: 'https://github.com/yourusername/portfolio-builder',
+    },
+    {
+      title: 'AI Chat Assistant',
+      description:
+        'Assistente de chat inteligente baseado em inteligência artificial e Flask.',
+      image: 'https://via.placeholder.com/300x200',
+      tags: ['Python', 'Flask', 'AI', 'Chatbot'],
+      githubUrl: 'https://github.com/yourusername/ai-chat-assistant',
+    },
+  ];
+
+  const [projects, setProjects] = useState(initialProjects);
+  const [showMore, setShowMore] = useState(false);
+
+  const handleShowMore = () => {
+    setProjects((prevProjects) => [...prevProjects, ...additionalProjects]);
+    setShowMore(true);
+  };
+
   return (
     <main className="min-h-screen bg-background">
       <HeroSection />
@@ -26,33 +123,29 @@ export default function Home() {
             Featured Projects
           </motion.h2>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-            <ProjectCard
-              title="Project 1"
-              description="A full-stack application built with React and Express"
-              image="https://images.unsplash.com/photo-1555066931-4365d14bab8c?auto=format&fit=crop&q=80&w=800"
-              tags={['React', 'Express', 'PostgreSQL']}
-              githubUrl="https://github.com/yourusername/project1"
-              liveUrl="https://project1.com"
-              index={0}
-            />
-            <ProjectCard
-              title="Project 2"
-              description="Mobile app developed with React Native"
-              image="https://images.unsplash.com/photo-1551650975-87deedd944c3?auto=format&fit=crop&q=80&w=800"
-              tags={['React Native', 'TypeScript']}
-              githubUrl="https://github.com/yourusername/project2"
-              index={1}
-            />
-            <ProjectCard
-              title="Project 3"
-              description="Backend API built with Python Flask"
-              image="https://images.unsplash.com/photo-1571171637578-41bc2dd41cd2?auto=format&fit=crop&q=80&w=800"
-              tags={['Python', 'Flask', 'Docker']}
-              githubUrl="https://github.com/yourusername/project3"
-              liveUrl="https://project3.com"
-              index={2}
-            />
+            {projects.map((project, index) => (
+              <ProjectCard
+                key={index}
+                title={project.title}
+                description={project.description}
+                image={project.image}
+                tags={project.tags}
+                githubUrl={project.githubUrl}
+                liveUrl={project.liveUrl}
+                index={index}
+              />
+            ))}
           </div>
+          {!showMore && (
+            <div className="text-center mt-12">
+              <button
+                onClick={handleShowMore}
+                className="bg-primary hover:bg-primary-dark text-white px-6 py-3 rounded-md"
+              >
+                Ver Mais Projetos
+              </button>
+            </div>
+          )}
         </div>
       </section>
 
